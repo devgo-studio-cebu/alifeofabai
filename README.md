f7587367d33b678e017bfefbca113291b5e3a3e8
devgo-studio-cebu <official+github@devgo.studio>
2024-10-14 22:18:37 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,36 @@
+# A Life Of A Bai
+
+## Prerequisites
+
+Before you begin, ensure the following are installed on your machine:
+
+- **Node.js**
+- **pnpm**
+
+## Getting Started
+
+To set up the project for development, follow these steps:
+
+1. Clone the repository:
+
+```bash
+git clone git@github.com:devgo-studio/alifeofabai.git
+```
+
+2. Navigate to the project directory:
+
+```bash
+cd alifeofabai/
+```
+
+3. Install the dependencies:
+
+```bash
+pnpm install
+```
+
+4. Start the development server:
+
+```bash
+pnpm dev
+```
---
+++ prettier.config.mjs
@@ -0,0 +1,30 @@
+/** @type {import("prettier").Config} */
+export default {
+  importOrder: [
+    "<TYPES>",
+    "<TYPES>^[.]",
+    "",
+    "<BUILTIN_MODULES>",
+    "^react$",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^[.]",
+    "",
+    "^(?!.*[.]css$)[./].*$",
+    ".css$",
+  ],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-astro",
+    "prettier-plugin-tailwindcss",
+  ],
+  semi: false,
+}
