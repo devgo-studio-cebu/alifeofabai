2b3e129824e9e4947177812088e6d9abefa835cc
dpswh-dev <lanutanenzo.developer@gmail.com>
2024-10-23 02:01:52 +0800

    feat(landing): install shadcn Carousel component

--- src/components/Songs.tsx
+++ src/components/Songs.tsx
@@ -1,29 +1,23 @@
-import { MusicVideo } from "./MusicVideo";
+import { MusicVideo } from "./MusicVideo"
 
-export function Songs(){
-    return  (
-        <section>
-            <div
-                className="m-0 flex h-lvh snap-y flex-col items-center justify-center bg-[#202020] p-0"
-            >
+export function Songs() {
+    return (
+        <section className="snap-y snap-mandatory">
+            <div className="m-0 flex h-lvh snap-y flex-col items-center justify-center bg-[#202020] p-0">
                 <MusicVideo
                     videoSource="/17b-edited-final.webm"
                     videoFormat="webm"
                 />
             </div>
 
-            <div
-                className="m-0 flex h-lvh snap-y flex-col items-center justify-center bg-[#303030] p-0"
-            >
+            <div className="m-0 flex h-lvh snap-y flex-col items-center justify-center bg-[#303030] p-0">
                 <MusicVideo
                     videoSource="/agad-edited.webm"
                     videoFormat="webm"
                 />
             </div>
 
-            <div
-                className="m-0 flex h-lvh snap-y flex-col items-center justify-center bg-[#404040] p-0"
-            >
+            <div className="m-0 flex h-lvh snap-y flex-col items-center justify-center bg-[#404040] p-0">
                 <MusicVideo
                     videoSource="/pasitib-edited.webm"
                     videoFormat="webm"
